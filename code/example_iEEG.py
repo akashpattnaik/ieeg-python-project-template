b099a76eca2754e4788292054d185e9a1526ed22
Akash Ranjan Pattnaik <pattnaik@pioneer.seas.upenn.edu>
2022-09-22 11:56:47 -0400

    added clean labels and clean up get ieeg function

--- code/example_iEEG.py
+++ code/example_iEEG.py
@@ -12,13 +12,15 @@ path = "/".join(os.path.abspath(os.getcwd()).split('/')[:-1])
 
 import json
 import numpy as np
-from get_iEEG_data import get_iEEG_data
-from plot_iEEG_data import plot_iEEG_data
-from line_length import line_length
-from get_iEEG_duration import get_iEEG_duration
+
+import tools
+# from get_iEEG_data import get_iEEG_data
+# from plot_iEEG_data import plot_iEEG_data
+# from line_length import line_length
+# from get_iEEG_duration import get_iEEG_duration
 
 # %%
-with open("../credentials.json") as f:
+with open("../credentials.json", 'r') as f:
     credentials = json.load(f)
     username = credentials['username']
     password = credentials['password']
@@ -29,31 +31,14 @@ stop_time_usec = 402800 * 1e6
 electrodes = ["LE10","LE11","LH01","LH02","LH03","LH04"]
 
 # %%
-data, fs = get_iEEG_data(username, password, iEEG_filename, start_time_usec, stop_time_usec, select_electrodes=electrodes)
+data, fs = tools.get_iEEG_data(username, password, iEEG_filename, start_time_usec, stop_time_usec, select_electrodes=electrodes)
 
 # %% Plot the data
 t_sec = np.linspace(start_time_usec, stop_time_usec, num=data.shape[0]) / 1e6
-fig, ax = plot_iEEG_data(data, t_sec)
+fig, ax = tools.plot_iEEG_data(data, t_sec)
 fig.set_size_inches(18.5, 10.5)
 ax.set_title(iEEG_filename)
 fig.show()
 plt.savefig("../figures/test.png")
 
-# %%
-win_size_sec = 5
-
-win_size_ind = int(win_size_sec * fs)
-
-start_range = np.arange(0, len(data), win_size_ind, dtype=int)
-
-ll_arr = np.zeros(len(start_range))
-for i, start_ind in enumerate(start_range):
-    ll_arr[i] = line_length(data[start_ind:(start_ind + win_size_ind)])
-
-fig, ax = plt.subplots()
-ax.plot(start_range/fs, ll_arr)
-ax.set_title("Line Length")
-ax.set_xlabel("Clip Time (sec)")
-# %%
-get_iEEG_duration(username, password, iEEG_filename)
 # %%
---
+++ code/tools/__init__.py
@@ -0,0 +1,3 @@
+from .get_iEEG_data import get_iEEG_data
+from .clean_labels import clean_labels
+from .plot_iEEG_data import plot_iEEG_data
